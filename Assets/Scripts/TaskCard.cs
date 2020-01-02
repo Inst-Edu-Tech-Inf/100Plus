4f15950755d3019d777f74c19ff3234a9494297f
pixldev <pixeloo@protonmail.com>
2020-01-02 14:40:49 +0100

    dodanie prototypu wykonywania zadan

---
+++ Assets/Scripts/TaskCard.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class TaskCard : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    int value;
+    int victoryPoints;
+
+    TextMeshProUGUI valueText;
+    TextMeshProUGUI victoryPointsText;
+    TextMeshProUGUI colorText;
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        if (pointerEventData.pointerEnter)
+        {
+            GetComponent<Outline>().enabled = true;
+            transform.Find("Drop Panel").gameObject.SetActive(true);
+        }
+    }
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        if (pointerEventData.pointerEnter)
+        {
+            GetComponent<Outline>().enabled = false;
+            transform.Find("Drop Panel").gameObject.SetActive(false);
+        }
+    }
+
+
+}
