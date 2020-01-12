726908185db55b7b4bfe71e7c1ce2f6b8b574da6
pixldev <pixeloo@protonmail.com>
2020-01-12 18:32:00 +0100

    dodanie dobierania kart przez skrypt

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,14 @@ public class GameManager : MonoBehaviour
 {   
     public GameObject activeCardSpace;
     public GameObject tasks;
+    public GameObject hands;
+    public GameObject playerCardPrefab;
+    public GameObject taskCardPrefab;
+
+    [Header("Game Settings"), SerializeField]
+    int playerCardsOnStart;
+    [SerializeField]
+    int taskCardsOnStart;
 
     GameObject activeCard;
 
@@ -26,5 +34,30 @@ public class GameManager : MonoBehaviour
             card.transform.Find("Drop Panel").gameObject.SetActive(false);
             activeCard = null;
         }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < playerCardsOnStart; i++)
+        {
+            DrawPlayerCard();
+        }
+
+        for (int i = 0; i < taskCardsOnStart; i++)
+        {
+            DrawTaskCard();
+        }
+    }
+
+    void DrawPlayerCard()
+    {
+        GameObject card = Instantiate(playerCardPrefab);
+        card.transform.SetParent(hands.transform);
+    }
+
+    void DrawTaskCard()
+    {
+        GameObject card = Instantiate(taskCardPrefab);
+        card.transform.SetParent(tasks.transform);
     }
 }
