2136cbf7e61edf7c802a9fb3085416282f0d2f1b
pixl <36799862+pixldev@users.noreply.github.com>
2020-05-12 00:32:42 +0200

    wieksze przyciski

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -11,12 +11,12 @@ using System;
 
 public class MenuButtons : MonoBehaviour
 {
-    [Header("Background"), SerializeField]
-    public Image backgroundImage;
+	[Header("Background"), SerializeField]
+	public Image backgroundImage;
 
-    void changeBackground()
-    {
-        /*string pom = "SplashScreen.jpg";
+	void changeBackground()
+	{
+		/*string pom = "SplashScreen.jpg";
         pom = System.IO.Path.Combine(Application.streamingAssetsPath, pom);
         byte[] pngBytes = System.IO.File.ReadAllBytes(pom);
 
@@ -31,122 +31,122 @@ public class MenuButtons : MonoBehaviour
 
         //backgroundImage.sprite = fromTex;
         GameObject.Find("Image").GetComponent<Image>().sprite = fromTex;*/
-        //GameObject.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>(SkinManager.instance.tla[SkinManager.instance.ActiveBackground].Name); 
-    }
+		//GameObject.Find("Image").GetComponent<Image>().sprite = Resources.Load<Sprite>(SkinManager.instance.tla[SkinManager.instance.ActiveBackground].Name); 
+	}
 
-    IEnumerator Foo(string text, float delay)
-    {
-        yield return new WaitForSeconds(delay);
+	IEnumerator Foo(string text, float delay)
+	{
+		yield return new WaitForSeconds(delay);
 
-        // code here
-        changeBackground();
+		// code here
+		changeBackground();
 
-    }
+	}
 
-    void Start()
-    {
-         // StartCoroutine(Foo("Text", 10)); 
-        //changeBackground();
-            
-    }
+	void Start()
+	{
+		// StartCoroutine(Foo("Text", 10)); 
+		//changeBackground();
 
-    void Awake()
-    {
-        
-    }
+	}
 
-    public void exitButton()
-    {
-        Application.Quit();
-    }
+	void Awake()
+	{
 
-    public void aboutButton()
-    {
-        SceneManager.LoadScene("About");
-    }
+	}
 
-    public void multiplayerButton()
-    {
-        SceneManager.LoadScene("Multiplayer");
-    }
+	public void exitButton()
+	{
+		Application.Quit();
+	}
 
-    public void skinButton()
-    {
-        SceneManager.LoadScene("Skins");
-    }
+	public void aboutButton()
+	{
+		SceneManager.LoadScene("About");
+	}
 
-    public void optionButton()
-    {
-        SceneManager.LoadScene("GameSetting");
-    }
+	public void multiplayerButton()
+	{
+		SceneManager.LoadScene("Multiplayer");
+	}
 
-    public void playButton()
-    {
-        SceneManager.LoadScene("Game");
-    }
+	public void skinButton()
+	{
+		SceneManager.LoadScene("Skins");
+	}
 
-    public void achievementButton()
-    {
-        SceneManager.LoadScene("Achievement");
-    }
+	public void optionButton()
+	{
+		SceneManager.LoadScene("GameSetting");
+	}
 
-  /*  void OnGUI()
-    {
-        const int ILE_ELEMENTOW_MENU = 6;
-        const int SZEROKOSC_POLA = 250;
-        int offset = 20;
-        int szerokoscPrzycisku = 100;
-        Scene scene;
-        scene = SceneManager.GetActiveScene();
+	public void playButton()
+	{
+		SceneManager.LoadScene("Game");
+	}
 
-        //sprawdzenie czy aktualna scena to menu zeby nie wyswietlac menu w grze
+	public void achievementButton()
+	{
+		SceneManager.LoadScene("Achievement");
+	}
 
-        if (scene == SceneManager.GetSceneByName("Menu"))
-        {
-            
-            GUIStyle style = new GUIStyle(GUI.skin.GetStyle("button"));
-            style.fontSize = 50;
-            szerokoscPrzycisku = Screen.height / (ILE_ELEMENTOW_MENU + 1);
-            offset = szerokoscPrzycisku / ILE_ELEMENTOW_MENU;
+	/*  void OnGUI()
+	  {
+		  const int ILE_ELEMENTOW_MENU = 6;
+		  const int SZEROKOSC_POLA = 250;
+		  int offset = 20;
+		  int szerokoscPrzycisku = 100;
+		  Scene scene;
+		  scene = SceneManager.GetActiveScene();
 
-            //wyswietlnie samych guzikow 
-            if (GUI.Button(new Rect(0, 0, SZEROKOSC_POLA, szerokoscPrzycisku), "TEST", style))
-            {
-                SceneManager.LoadScene("Test");
-            }
+		  //sprawdzenie czy aktualna scena to menu zeby nie wyswietlac menu w grze
+
+		  if (scene == SceneManager.GetSceneByName("Menu"))
+		  {
+
+			  GUIStyle style = new GUIStyle(GUI.skin.GetStyle("button"));
+			  style.fontSize = 50;
+			  szerokoscPrzycisku = Screen.height / (ILE_ELEMENTOW_MENU + 1);
+			  offset = szerokoscPrzycisku / ILE_ELEMENTOW_MENU;
+
+			  //wyswietlnie samych guzikow 
+			  if (GUI.Button(new Rect(0, 0, SZEROKOSC_POLA, szerokoscPrzycisku), "TEST", style))
+			  {
+				  SceneManager.LoadScene("Test");
+			  }
 
 
-            if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 0, SZEROKOSC_POLA, szerokoscPrzycisku), "Start", style))
-            {
-                SceneManager.LoadScene("Game");
-            }
-#if !HTML5 
-            if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, szerokoscPrzycisku + offset, SZEROKOSC_POLA, szerokoscPrzycisku), "Multiplayer", style))
-            {
-                SceneManager.LoadScene("Multiplayer");
-            }
+			  if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 0, SZEROKOSC_POLA, szerokoscPrzycisku), "Start", style))
+			  {
+				  SceneManager.LoadScene("Game");
+			  }
+  #if !HTML5 
+			  if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, szerokoscPrzycisku + offset, SZEROKOSC_POLA, szerokoscPrzycisku), "Multiplayer", style))
+			  {
+				  SceneManager.LoadScene("Multiplayer");
+			  }
 
-            if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 2 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "Options", style))
-            {
-                //GUI.Slider()
-                SceneManager.LoadScene("GameSetting");
-            }
+			  if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 2 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "Options", style))
+			  {
+				  //GUI.Slider()
+				  SceneManager.LoadScene("GameSetting");
+			  }
 
-            if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 3 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "Skins", style))
-            {
-                SceneManager.LoadScene("Skins");
-            }
+			  if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 3 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "Skins", style))
+			  {
+				  SceneManager.LoadScene("Skins");
+			  }
 
-            if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 4 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "About", style))
-            {
-                SceneManager.LoadScene("About");
-            }
-#endif
-            if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 5 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "Quit", style))
-            {
-                w
-            }
-        }
-    }
-*/
+			  if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 4 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "About", style))
+			  {
+				  SceneManager.LoadScene("About");
+			  }
+  #endif
+			  if (GUI.Button(new Rect(Screen.width - SZEROKOSC_POLA, 5 * (szerokoscPrzycisku + offset), SZEROKOSC_POLA, szerokoscPrzycisku), "Quit", style))
+			  {
+				  w
+			  }
+		  }
+	  }
+  */
 }
--- Assets/Scripts/MultiplayerUI.cs
+++ Assets/Scripts/MultiplayerUI.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
 using Mirror;
 
-public class MultiplayerUI : NetworkBehaviour
+public class MultiplayerUI : MonoBehaviour
 {
-    public NetworkRoomManager roomManager;
+	public RoomPlayer roomPlayer;
+	public NetworkRoomManager roomManager
+	{
+		get => GameObject.FindGameObjectWithTag("Room Manager").GetComponent<NetworkRoomManager>();
+		set
+		{
+			roomManager = value;
+		}
+	}
 
-    public void LocalPlayerReady()
-    {
-        NetworkRoomPlayer localPlayer = ClientScene.localPlayer.GetComponent<NetworkRoomPlayer>();
-        
-        if (!localPlayer.readyToBegin)
-        {
-            localPlayer.readyToBegin = true;
-        }
-        else
-        {
-            localPlayer.readyToBegin = false;
-        }
-    }
+	public void CreateGame()
+	{
+		roomManager.StartHost();
+	}
+
+	public void JoinGame()
+	{
+		roomManager.StartClient();
+	}
+
+	public void Ready()
+	{
+		if (!roomPlayer.readyToBegin)
+		{
+			roomPlayer.readyToBegin = true;
+			roomManager.CheckReadyToBegin();
+		}
+		else
+		{
+			roomPlayer.readyToBegin = false;
+		}
+	}
 }
