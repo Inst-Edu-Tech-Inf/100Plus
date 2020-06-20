62b4b2e41194e6a64520a32761b8f33a57d28e03
pixl <36799862+pixldev@users.noreply.github.com>
2020-06-20 03:15:41 +0200

    Większe przyciski w menu

--- Assets/Scripts/MultiplayerUI.cs
+++ Assets/Scripts/MultiplayerUI.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using Mirror;
 using Mirror.Discovery;
+using TMPro;
 
 public class MultiplayerUI : MonoBehaviour
 {
@@ -14,10 +16,58 @@ public class MultiplayerUI : MonoBehaviour
 		}
 	}
 	public NetworkDiscovery discovery;
+	public Button listButton;
+	public ScrollRect list;
+	public TextMeshProUGUI readyText;
 
     private void Start()
     {
 		discovery = GameObject.FindGameObjectWithTag("Room Manager").GetComponent<NetworkDiscovery>();
+	}
+
+	public void OnServerFound(ServerResponse info)
+    {
+		print("server found " + info.uri.Host);
+		if (list.content.childCount >= 1)
+        {
+			foreach (Transform go in list.content.transform)
+			{
+				if (go.GetComponentInChildren<Text>().text != info.uri.Host)
+				{
+					AppendListButton(info);
+				}
+			}
+		}
+		else
+        {
+			AppendListButton(info);
+			AppendListButton(info);
+			AppendListButton(info);
+			AppendListButton(info);
+			AppendListButton(info);
+			AppendListButton(info);
+		}
+    }
+
+	void AppendListButton(ServerResponse info)
+    {
+		float btnY = list.content.transform.position.y;
+		if (list.content.childCount >= 1)
+        {
+			btnY = list.content.GetChild(list.content.childCount - 1).transform.position.y - 35f;
+        }
+        
+		GameObject btn = Instantiate(listButton.gameObject);
+		btn.GetComponentInChildren<Text>().text = info.uri.Host;
+		btn.GetComponent<Button>().onClick.AddListener(delegate { ListButtonOnClick(info); });
+		btn.transform.SetParent(list.content, false);
+		btn.transform.position = new Vector2(list.transform.position.x, btnY);
+	}
+
+	void ListButtonOnClick(ServerResponse info)
+    {
+		roomManager.networkAddress = info.uri.Host;
+		roomManager.StartClient();
     }
 
     public void CreateGame()
