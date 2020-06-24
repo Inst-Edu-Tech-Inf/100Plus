da2cc446c535b630e6b43c074e64a11ca90f38d0
pixl <36799862+pixldev@users.noreply.github.com>
2020-06-24 19:01:06 +0200

    Liczba graczy i gotowych graczy w poczekalni

--- Assets/Scripts/NetRoom.cs
+++ Assets/Scripts/NetRoom.cs
@@ -1,7 +1,32 @@
 ﻿using UnityEngine;
 using Mirror;
+using TMPro;
+using System.Collections.Generic;
 
 public class NetRoom : NetworkRoomManager
 {
-    
+    TextMeshProUGUI readyText;
+    int readyPlayers = 0;
+
+    public override void Start()
+    {
+        readyText = GameObject.FindGameObjectWithTag("Ready Text").GetComponent<TextMeshProUGUI>();
+        base.OnStartHost();
+    }
+
+    private void Update()
+    {
+        readyPlayers = 0;
+        foreach (NetworkRoomPlayer player in this.roomSlots)
+        {
+            if (player.readyToBegin)
+                readyPlayers++;
+        }
+        UpdateReadyText();
+    }
+
+    public void UpdateReadyText()
+    {
+        readyText.text = $"Ready {readyPlayers} / {this.roomSlots.Count}";
+    }
 }
